df7daba5df7760af996c22682a0b2ae41662cd65
m-shogo <60862647+m-shogo@users.noreply.github.com>
2026-08-22 23:22:54 +0900

    feat(opening): wire optional J-cut audio layer into Opening V1

--- motion-studio/src/OpeningV1Root.tsx
+++ motion-studio/src/OpeningV1Root.tsx
@@ -1,12 +1,20 @@
 import {Composition} from 'remotion';
 import {OpeningV1} from './compositions/opening/OpeningV1';
+import {OpeningV1AudioLayer} from './compositions/opening/OpeningV1AudioLayer';
 import {openingV1TotalSec} from './data/openingV1';
 import {video} from './data/theme';
+
+const OpeningV1Composition = () => (
+  <>
+    <OpeningV1 />
+    <OpeningV1AudioLayer />
+  </>
+);
 
 export const OpeningV1Root = () => (
   <Composition
     id="OpeningV1"
-    component={OpeningV1}
+    component={OpeningV1Composition}
     durationInFrames={openingV1TotalSec * video.fps}
     fps={video.fps}
     width={video.width}
