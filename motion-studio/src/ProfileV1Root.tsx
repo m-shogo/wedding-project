7c392f0012054d84c5115f73f02a4ce3f224a71a
m-shogo <60862647+m-shogo@users.noreply.github.com>
2026-08-27 11:49:15 +0900

    movie: register Profile V1 real-media preview

--- motion-studio/src/ProfileV1Root.tsx
+++ motion-studio/src/ProfileV1Root.tsx
@@ -1,6 +1,7 @@
 import {Composition} from 'remotion';
 import {ProfileV1DeparturePreview} from './compositions/profile/ProfileV1DeparturePreview';
 import {ProfileV1FullStructurePreview} from './compositions/profile/ProfileV1FullStructurePreview';
+import {ProfileV1RealMediaPreview} from './compositions/profile/ProfileV1RealMediaPreview';
 import {video} from './data/theme';
 
 export const ProfileV1Root = () => (
@@ -16,6 +17,14 @@ export const ProfileV1Root = () => (
     <Composition
       id="ProfileV1FullStructurePreview"
       component={ProfileV1FullStructurePreview}
+      durationInFrames={30 * video.fps}
+      fps={video.fps}
+      width={video.width}
+      height={video.height}
+    />
+    <Composition
+      id="ProfileV1RealMediaPreview"
+      component={ProfileV1RealMediaPreview}
       durationInFrames={30 * video.fps}
       fps={video.fps}
       width={video.width}
